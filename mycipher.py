f49aa75ea2fb312349bae9e300bb55d579a0e2ca
Noble Adike <noble.adike@bison.howard.edu>
2025-04-17 23:02:56 +0000

    Wrote the first part of the code to encrypt the letters

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,18 @@
-#empty for now
+import sys
+
+shift = int(sys.argv[1])
+
+text = sys.stdin.read()
+
+message = ""
+
+text = text.upper()
+for char in text:
+
+    if char.isalpha():
+        shifted = ord(char) + shift
+        if shifted > ord("Z"):
+            shifted -= 26
+        new_char = chr(shifted)
+        message+= new_char
+
